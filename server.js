d274df19fb98ec367371dfa80bf2c73ed727b039
bliff182 <michael.bither@gmail.com>
2020-01-24 06:55:32 -0500

    models added, /scrape route working

--- server.js
+++ server.js
@@ -20,6 +20,85 @@ const MONGODB_URI =
 
 mongoose.connect(MONGODB_URI);
 
+app.get('/scrape', (req, res) => {
+	axios.get('https://www.theonion.com/').then(response => {
+		let $ = cheerio.load(response.data);
+		$('article').each((i, element) => {
+			let result = {};
+
+			result.headline = $(element)
+				.find('h4')
+				.text()
+				.trim();
+			result.summary = $(element)
+				.find('p')
+				.text()
+				.trim();
+			result.url = $(element)
+				.find('a')
+				.attr('href');
+
+			// if (result.headline && result.summary && result.url) {
+			if (result.headline && result.url) {
+				db.Article.create(result)
+					.then(dbArticle => {
+						console.log(dbArticle);
+					})
+					.catch(err => {
+						console.log(err);
+					});
+			}
+
+			// db.Article.create(result)
+			// 	.then(dbArticle => {
+			// 		console.log(dbArticle);
+			// 	})
+			// 	.catch(err => {
+			// 		console.log(err);
+			// 	});
+		});
+		res.send('Scraped.');
+	});
+});
+
+app.get('/articles', (req, res) => {
+	db.Article.find({})
+		.then(dbArticle => {
+			res.json(dbArticle);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+});
+
+app.get('/aritcles/:id', (req, res) => {
+	db.Article.findOne({ _id: req.params.id })
+		.populate('note')
+		.then(dbArticle => {
+			res.json(dbArticle);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+});
+
+app.post('/articles/:id', (req, res) => {
+	db.Note.create(req.body)
+		.then(dbNote => {
+			return dbArticle.findOneAndUpdate(
+				{ _id: req.params.id },
+				{ node: dbNote._id },
+				{ new: true }
+			);
+		})
+		.then(dbArticle => {
+			res.json(dbArticle);
+		})
+		.catch(err => {
+			res.json(err);
+		});
+});
+
 // Start the server
 app.listen(PORT, () => {
 	console.log(`App running on port ${PORT}!`);
